e34f1f18ea7a2f0e1c3ff7c088f190de7d41c28f
Bruno Lovric <brunoolovric@gmail.com>
2022-04-07 00:54:30 +0200

    Add prop to obj

---
+++ ex06/src/index.js
@@ -0,0 +1,16 @@
+var lion = {
+  name: "Simba",
+  legs: 4,
+  tails: 1
+};
+
+function myFunction(propName, propValue) {
+  lion[propName] = propValue;
+  return lion;
+}
+
+myFunction("roar", "roar-roar");
+
+console.log(myFunction("roar", "roar-roar"));
+
+module.exports = myFunction;
