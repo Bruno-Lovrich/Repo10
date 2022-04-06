6f84dcdb8652edf9ec5c1b975a1fda3631dff5b8
Bruno Lovric <brunoolovric@gmail.com>
2022-04-07 01:01:02 +0200

    Delete obj prop

---
+++ ex07/src/index.js
@@ -0,0 +1,17 @@
+function myFunction(propName) {
+  var lion = {
+      name: "Simba",
+      legs: 4,
+      tails: 1,
+      roar: "roar-roar"
+  };
+
+  delete lion.roar;
+  return lion;
+}
+
+myFunction("roar");
+
+console.log(myFunction("roar"));
+
+module.exports = myFunction;
